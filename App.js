ca380bbed0a2b7548055a9a43c414492b242c740
早川正午 <pigiipanku@gmail.com>
2019-06-16 16:11:47 +0900

    style-memo-detail screen

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import { StyleSheet, View } from 'react-native';
 
 import Appbar from './src/components/Appbar';
 import MemoListScreen from './src/screens/MemoListScreen'
+import MemoDetailScreen from './src/screens/MemoDetailScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Appbar />
-      <MemoListScreen />
+      <MemoDetailScreen />
     </View>
   );
 }
