4834268a212af3cb5a39ca169a0dc59e86d76df7
早川正午 <pigiipanku@gmail.com>
2019-06-16 14:53:37 +0900

    Create Memo list Screen

--- App.js
+++ App.js
@@ -2,18 +2,14 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Appbar from './src/components/Appbar';
-import MemoList from './src/components/MemoList.js';
-import CircleButton from './src/elements/CircleButton'
+import MemoListScreen from './src/screens/MemoListScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
-
       <Appbar />
-      <MemoList />
-      <CircleButton>+</CircleButton>
-
-  </View>
+      <MemoListScreen />
+    </View>
   );
 }
 
